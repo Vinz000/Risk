2f594a88fb448db49d82fec975d04f7f587d14f0
logiczsniper <lczernel@gmail.com>
2021-03-06 10:46:11 +0000

    refactor(common): dice and created more tests

--- test/common/DiceTest.java
+++ test/common/DiceTest.java
@@ -1,44 +1,89 @@
 package common;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
-    private Dice dice;
-
-    @BeforeEach
-    void setUp() {
-        dice = new Dice();
-    }
 
     @Test
-    void testGetNextDiceShouldReturnListOfDiceRollInOrder() {
-        List<Integer> dice1 = dice.getNextDice(10);
+    void testRollReturnsIntegersInDescendingOrder() {
+        List<Integer> rolledDice = Dice.roll(10);
+        Predicate<Integer> isGreaterThanNext = index -> {
+            int currentDie = rolledDice.get(index);
+            int nextDie = rolledDice.size() > index + 1 ?
+                    rolledDice.get(index + 1) :
+                    -1;
+            return currentDie >= nextDie;
+        };
 
-        for (int i = 1; i < dice1.size() - 1; i++) {
-            assertTrue(dice1.get(i + 1) <= dice1.get(i));
-            assertTrue(dice1.get(i) >= 1);
-            assertTrue(dice1.get(i) <= 6);
-        }
-
-    }
-
-    @Test
-    void testGetRollSumShouldReturnSumOfDiceRoll() {
-        for (int i = 0; i < 36; i++) {
-            int diceSum = dice.getRollSum(2);
-            assertTrue(diceSum >= 2 && diceSum <= 12);
+        for (int die : rolledDice) {
+            assertTrue(isGreaterThanNext.test(die));
         }
     }
 
     @Test
-    void testToStringShouldReturnDiceRollAsFormattedString() {
-        List<Integer> dice1 = dice.getNextDice(2);
+    void testRollReturnsIntegersInRangeOneToSix() {
+        List<Integer> rolledDice = Dice.roll(10);
+        Predicate<Integer> isInRangeOneToSix = number -> number >= 1 && number <= 6;
 
-        assertEquals(dice.toString(), String.format("[ %d ] [ %d ] ", dice1.get(0), dice1.get(1)));
+        for (int die : rolledDice) {
+            assertTrue(isInRangeOneToSix.test(die));
+        }
     }
+
+    @Test
+    void testRollBreaksReferenceOnConsecutiveRolls() {
+        List<Integer> firstRolledDice = Dice.roll(100000);
+        List<Integer> secondRolledDice = Dice.roll(100000);
+
+        // Note: has slim chance of failing!
+        assertNotEquals(firstRolledDice, secondRolledDice);
+    }
+
+    @Test
+    void testRollReturnsDiceCountNumberOfIntegers() {
+        int diceCount = 1000;
+        List<Integer> rolledDice = Dice.roll(diceCount);
+
+        assertEquals(diceCount, rolledDice.size());
+    }
+
+    @Test
+    void testRollZeroDiceCountThrowsAssertionError() {
+        assertThrows(AssertionError.class, () -> Dice.roll(0));
+    }
+
+    @Test
+    void testRollNegativeDiceCountThrowsAssertionError() {
+        assertThrows(AssertionError.class, () -> Dice.roll(-1));
+    }
+
+    @Test
+    void testSumDiceReturnsCorrectSum() {
+        List<Integer> rolledDice = Dice.roll(10);
+        int sum = 0;
+
+        for (int die : rolledDice) {
+            sum += die;
+        }
+
+        assertEquals(sum, Dice.sumDice(rolledDice));
+    }
+
+    @Test
+    void testSumDiceNullRolledDiceThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> Dice.sumDice(null));
+    }
+
+    // TODO: revise test after Vincent creates rich text module (affecting this).
+//    @Test
+//    void testToStringShouldReturnDiceRollAsFormattedString() {
+//        List<Integer> dice1 = dice.getNextDice(2);
+//
+//        assertEquals(dice.toString(), String.format("[ %d ] [ %d ] ", dice1.get(0), dice1.get(1)));
+//    }
 }
